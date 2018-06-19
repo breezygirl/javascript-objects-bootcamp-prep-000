32853ed702d09278df745377d5eb392a6bf8cc79
Beerscheba C <designergo@aol.com>
2018-06-19 13:55:19 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,13 +6,17 @@ var playlist ={
 /*to.contain.all.keys({'Slowdive': 'Alison', 'My Bloody Valentine': 'Sometimes', 'Phil Ochs': "Here's to the State of Mississippi"})
     })*/
 function updatePlaylist(){
+  playlist;
 
   {artistName: 'My Bloody Valentine'
  artistName:'Phil Ochs'}
  
  { songTitle:'Sometimes'
   songTitle:"Here's to the State of Mississippi"
-}
+
+return updatePlaylist
+   
+ }
 
 
 function removeFromPlaylist(){)
