d14faef7b7708c4ddc20b6ee65022f3b93b2f3a4
Beerscheba C <designergo@aol.com>
2018-06-19 16:20:39 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,21 +1,18 @@
 
 var playlist ={
-  ArtistName:'Slowdive',
-  songTitle: 'Alison'}
-/*var playlist ={
   
-  Slowdive :'Alison'*/
+  Slowdive :'Alison'
   }
 /*to.contain.all.keys({'Slowdive': 'Alison', 'My Bloody Valentine': 'Sometimes', 'Phil Ochs': "Here's to the State of Mississippi"})
     })*/
- /* function updatedPlaylist ( playlist,artistName, SongTitle){
+  function updatedPlaylist ( playlist,artistName, SongTitle){
     var updatedplaylist = { 
       'My Bloody Valentine': 'sometimes',
       'Phil Ochs': "here's to the sate of Mississippi",
       Slowdive: 'Alison'}
     }
     return updatedPlaylist
-  }*/
+  }
  
    
  
