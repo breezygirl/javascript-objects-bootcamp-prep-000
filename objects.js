d1c5643c25cb1d0d5bedeb517e35b176a2fbffa2
Beerscheba C <designergo@aol.com>
2018-06-19 14:30:51 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,7 +6,7 @@ var playlist ={
 /*to.contain.all.keys({'Slowdive': 'Alison', 'My Bloody Valentine': 'Sometimes', 'Phil Ochs': "Here's to the State of Mississippi"})
     })*/
 function updatePlaylist(playlist, artistName,songTitle) {
- playlist;
+var playlist;
   
   {artistName: 'phil Ochs' 
   songTitle : "Here's to the State of Mississippi"}
