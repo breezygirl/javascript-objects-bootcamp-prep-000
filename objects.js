b04cad00d7fae464b1a684029daa1466dd030a1b
Beerscheba C <designergo@aol.com>
2018-06-19 15:51:33 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,7 +5,7 @@ var playlist ={
 /*to.contain.all.keys({'Slowdive': 'Alison', 'My Bloody Valentine': 'Sometimes', 'Phil Ochs': "Here's to the State of Mississippi"})
     })*/
 
- var updatedPlaylist = [{ Slowdive:'Alison'},
+  function updatedPlaylist () { Slowdive:'Alison'},
  {My Blood Valentine:'Sometimes'),
  
 { Phil Ochs:"Here's to the State of Mississippi "}]
