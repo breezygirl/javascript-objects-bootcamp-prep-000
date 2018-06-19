336b7401bc7055157bbc0a9c82f11fe77185e5ba
Beerscheba C <designergo@aol.com>
2018-06-19 17:32:26 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,6 +2,8 @@
 var playlist ={
   
   Slowdive :'Alison'
+  
+  
   }
 /*to.contain.all.keys({'Slowdive': 'Alison', 'My Bloody Valentine': 'Sometimes', 'Phil Ochs': "Here's to the State of Mississippi"})
     })*/
