5cbf5b506a410ef647d362f7f4fd0c963449f628
Beerscheba C <designergo@aol.com>
2018-06-19 16:24:56 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,13 +5,9 @@ var playlist ={
   }
 /*to.contain.all.keys({'Slowdive': 'Alison', 'My Bloody Valentine': 'Sometimes', 'Phil Ochs': "Here's to the State of Mississippi"})
     })*/
-  function updatedPlaylist ( playlist,artistName, SongTitle){
-    var updatedplaylist = { 
-      'My Bloody Valentine': 'sometimes',
-      'Phil Ochs': "here's to the sate of Mississippi",
-      Slowdive: 'Alison'}
-    }
-    return updatedPlaylist
+  function updatedPlaylist ( playlist, artistName, SongTitle){
+   playlist: Slowdive
+    
   }
  
    
