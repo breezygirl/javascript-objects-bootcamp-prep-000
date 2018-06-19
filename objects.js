39feac3621574409c731368e68ecdc3099ccf32d
Beerscheba C <designergo@aol.com>
2018-06-19 16:19:36 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -8,14 +8,14 @@ var playlist ={
   }
 /*to.contain.all.keys({'Slowdive': 'Alison', 'My Bloody Valentine': 'Sometimes', 'Phil Ochs': "Here's to the State of Mississippi"})
     })*/
-  function updatedPlaylist ( playlist,artistName, SongTitle){
+ /* function updatedPlaylist ( playlist,artistName, SongTitle){
     var updatedplaylist = { 
       'My Bloody Valentine': 'sometimes',
       'Phil Ochs': "here's to the sate of Mississippi",
       Slowdive: 'Alison'}
     }
     return updatedPlaylist
-  }
+  }*/
  
    
  
