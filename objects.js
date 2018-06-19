11022bf3f5be03136dfb26802e6aedc58f6f5d9e
Beerscheba C <designergo@aol.com>
2018-06-19 16:56:35 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -12,8 +12,7 @@ var playlist ={
  
    
  function removeFromPlaylist(playlist, artistName){
- playlist[artistName] = Slowdive;
- 
+ playlist[artistName] 
  artistName = Slowdive
 delete artistName.Slowdive;
  
