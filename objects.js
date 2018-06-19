06d5b0b8a8568e60e39a63b588a2ddd1c5fe89af
Beerscheba C <designergo@aol.com>
2018-06-19 16:25:16 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,7 +6,7 @@ var playlist ={
 /*to.contain.all.keys({'Slowdive': 'Alison', 'My Bloody Valentine': 'Sometimes', 'Phil Ochs': "Here's to the State of Mississippi"})
     })*/
   function updatedPlaylist ( playlist, artistName, SongTitle){
-   playlist: Slowdive
+   playlist[artistName] = songTitle
     
   }
  
